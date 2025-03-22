2fe528ee41f690bb40ff59b7a3625c1327d7a2fc
thisguyblink <patrickliamsmith@gmail.com>
2025-03-22 14:45:48 -0700

    Basic Frontend done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { writable } from 'svelte/store';
+	let files = $state();
+    let dialog;
+    let name = $state("");
+
+	$effect(() => {
+		if (files) {
+			// Note that `files` is of type `FileList`, not an Array:
+			// https://developer.mozilla.org/en-US/docs/Web/API/FileList
+			console.log(files);
+
+            if (name === "" && files.length > 0 && files[0]) {
+                name = files[0].name;  // Update the name store with the file's name
+            }
+
+			for (const file of files) {
+				console.log(`${file.name}: ${file.size} bytes`);
+			}
+		}
+	});
+
+    function sendDoc() {
+        console.log("sent Doc");
+    }
+    function showPop() {
+        dialog.showModal();
+    }
+    function closePop() {
+        dialog.close();
+    }
+
+</script>
+
+<label for="lab">Upload lab results:</label>
+<input accept="application/pdf" bind:files id="lab" name="lab" type="file" />
+
+<button onclick={showPop}> Submit Lab </button>
+
+<div class = "results">
+    <dialog id="dresult" bind:this={dialog}>
+        <button type="button" id="exit" onclick={() => {
+            closePop();
+        }}>X</button>
+        <br>
+        <div class="popup">
+            <p>Is this the correct file {name}?</p>
+            <button 
+            onclick={() => { sendDoc(); closePop(); }}>
+                Yes
+            </button>
+            <button onclick={closePop}>No</button>
+        </div>
+    </dialog>
+</div>
+
+
+
